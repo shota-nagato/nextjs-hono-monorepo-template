615f24375a729a7456f699c2294fb723b58ba837
shota-nagato <shota.nagato@gmail.com>
2025-11-09 22:54:01 +0900

    add open-api

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,12 +1,29 @@
-import { Hono } from 'hono'
+import { OpenAPIHono } from '@hono/zod-openapi'
+import { swaggerUI } from '@hono/swagger-ui'
 import { corsMiddleware } from './middlewares/cors'
+import { auth } from './lib/better-auth'
 
-const app = new Hono()
+const app = new OpenAPIHono<{
+  Bindings: CloudflareBindings
+}>()
 
 app.use('/*', corsMiddleware)
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.on(['GET', 'POST'], '/api/v1/auth/*', (c) => auth(c.env).handler(c.req.raw))
+
+app.doc('/doc', {
+  openapi: '3.0.0',
+  info: {
+    title: 'API Documentation',
+    version: '1.0.0',
+  },
 })
+
+app.get(
+  '/ui',
+  swaggerUI<{ Bindings: CloudflareBindings }>({
+    url: '/doc',
+  }),
+)
 
 export default app
