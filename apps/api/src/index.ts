40c20baf308168a0abc0e0feadec1a59e8afc8bc
shota-nagato <shota.nagato@gmail.com>
2025-11-12 10:05:56 +0900

    add logger

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,14 +2,19 @@ import { OpenAPIHono } from '@hono/zod-openapi'
 import { swaggerUI } from '@hono/swagger-ui'
 import { corsMiddleware } from './middlewares/cors'
 import { auth } from './lib/better-auth'
+import { logger } from 'hono/logger'
 
 const app = new OpenAPIHono<{
   Bindings: CloudflareBindings
 }>()
 
+// TODO: 環境によってログ設定を変える
+app.use('*', logger())
 app.use('/*', corsMiddleware)
 
 app.on(['GET', 'POST'], '/api/v1/auth/*', (c) => auth(c.env).handler(c.req.raw))
+
+app.get('/health', (c) => c.json({ status: 'ok' }))
 
 export const routes = app
 
